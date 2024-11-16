824bf90682ea50f89e32f7d810d1f77659c2871d
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-11-16 17:51:19 +0900

    init: 프로젝트 초기세팅

---
+++ amazon/.eslintrc.cjs
@@ -0,0 +1,61 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'import'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index'], 'object', 'type'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: 'react*',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@tanstack/*',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@apis/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '{@types,@schema}',
+            group: 'type',
+            position: 'before',
+          },
+        ],
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        'newlines-between': 'always',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.app.json',
+      },
+    },
+  },
+};
---
+++ amazon/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>Amazon</h1>
+      <p className="read-the-docs">웨비들 화이팅🌊</p>
+    </>
+  );
+}
+
+export default App;
