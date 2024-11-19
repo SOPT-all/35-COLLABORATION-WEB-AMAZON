677241c0c7bc4c34d8a4c92e19f3c8f28f6d65bf
daahyunk <ekgusdl0408@naver.com>
2024-11-20 00:18:13 +0900

    fix: SVG 파일을 public 디렉토리로 이동 및 업데이트 (#8)

--- amazon/src/assets/svg/IcBreadArrow.tsx
+++ amazon/src/assets/svg/IcBreadArrow.tsx
@@ -1,7 +1,7 @@
 import type { SVGProps } from 'react';
-const SvgIcnBreadArrow = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="none" {...props}>
+const SvgIcBreadArrow = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16" {...props}>
     <path fill="#848688" d="M12 7.31v1.38L4 12v-1.552l6.257-2.43L4 5.585V4z" />
   </svg>
 );
-export default SvgIcnBreadArrow;
+export default SvgIcBreadArrow;
