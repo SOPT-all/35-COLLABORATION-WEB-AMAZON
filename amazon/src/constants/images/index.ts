376e29455672b3a2af933fbe430c408afc3c2a65
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-11-21 23:47:16 +0900

    design: 상품 카드 컴포넌트 수정 (#22)

---
+++ amazon/src/constants/images/index.ts
@@ -0,0 +1,6 @@
+export const ProductCardImage1 = '/image/productCard/img_product_card1.png';
+export const ProductCardImage2 = '/image/productCard/img_product_card2.png';
+export const ProductCardImage3 = '/image/productCard/img_product_card3.png';
+export const ProductCardImage4 = '/image/productCard/img_product_card4.png';
+export const ProductCardImage5 = '/image/productCard/img_product_card5.png';
+export const ProductCardImage6 = '/image/productCard/img_product_card6.png';
