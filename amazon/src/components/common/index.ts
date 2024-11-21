28ad0c26e2a894aefa4e1f3a8f4593b2947e9f1a
imhwarang <imhwarang4@naver.com>
2024-11-22 03:12:34 +0900

    refactor: index.ts 에러 해결

--- amazon/src/components/common/index.ts
+++ amazon/src/components/common/index.ts
@@ -1,11 +1,19 @@
 import BottomLoginRecommend from './BottomLoginRecommend/BottomLoginRecommend';
 import BottomRecommend from './BottomRecommend/BottomRecommend';
-import MainCard from './MainCard/MainCard';
-
 import Breadcrumb from './Breadcrumb/Breadcrumb';
+import MainCard from './MainCard/MainCard';
 import MainCarousel from './MainCarousel/MainCarousel';
 import ProductList from './ProductList/ProductList';
 import SearchBar from './SearchBar/SearchBar';
 import SortingBar from './SortingBar/SortingBar';
 
-export { MainCarousel, Breadcrumb, SearchBar, ProductList, MainCard, BottomLoginRecommend, BottomRecommend, SortingBar };
+export {
+  MainCarousel,
+  Breadcrumb,
+  SearchBar,
+  ProductList,
+  MainCard,
+  BottomLoginRecommend,
+  BottomRecommend,
+  SortingBar,
+};
