4a4be8a1fb2bf3f1e91da3c5e61033d4c9509918
imhwarang <imhwarang4@naver.com>
2024-11-22 03:22:03 +0900

    refactor: 이미지 경로 수정

--- amazon/src/constants/images/index.ts
+++ amazon/src/constants/images/index.ts
@@ -1,9 +1,9 @@
-const ProductCardImage1 = '/image/productCard/img_product_card1.png';
-const ProductCardImage2 = '/image/productCard/img_product_card2.png';
-const ProductCardImage3 = '/image/productCard/img_product_card3.png';
-const ProductCardImage4 = '/image/productCard/img_product_card4.png';
-const ProductCardImage5 = '/image/productCard/img_product_card5.png';
-const ProductCardImage6 = '/image/productCard/img_product_card6.png';
+const ProductCardImage1 = '/image/productCard/img_card1.png';
+const ProductCardImage2 = '/image/productCard/img_card2.png';
+const ProductCardImage3 = '/image/productCard/img_card3.png';
+const ProductCardImage4 = '/image/productCard/img_card4.png';
+const ProductCardImage5 = '/image/productCard/img_card5.png';
+const ProductCardImage6 = '/image/productCard/img_card6.png';
 
 const productCardImages = [
   ProductCardImage1,
