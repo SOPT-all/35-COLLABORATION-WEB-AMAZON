015538a98eedd4a50d82eeb4d11dd95025b1e5f8
Rinakk <bloomcott@u.sogang.ac.kr>
2024-11-23 14:05:27 +0900

    feat: 메인 페이지 구현

---
+++ amazon/src/pages/home/Home.style.ts
@@ -0,0 +1,34 @@
+import { css } from '@emotion/react';
+
+export const homeStyle = css`
+  width: 100%;
+  overflow-x: hidden;
+  
+`;
+
+export const headerStyle = css`
+  position: relative;
+  top: 0;
+`;
+
+export const sectionStyle = css`
+  width: 100%;
+  position: absolute;
+  top: 35rem;
+
+`;
+
+export const cardStyle = css`
+  gap: 1.4rem;
+  display: flex;
+  flex-direction: row;
+  margin: 0 2.2rem 3rem 2.2rem;
+  display: flex;
+`;
+
+export const scrollStyle = css`
+  margin: 0 2.2rem 3rem 2.2rem;
+`;
+export const loginRecommendStyle = css`
+  margin-bottom: 3rem;
+`;
--- amazon/src/pages/home/Home.tsx
+++ amazon/src/pages/home/Home.tsx
@@ -1,11 +1,44 @@
-import { Outlet } from 'react-router-dom';
+import { MainCarousel, BottomLoginRecommend, MainCard, HorizontalScroll, Header } from '@components';
+import { images } from '@constants';
+
+import { homeStyle, sectionStyle, scrollStyle, cardStyle, loginRecommendStyle, headerStyle } from './Home.style';
+import mainCardImage2 from '../../assets/images/img_main_card10.svg';
+import mainCardImage3 from '../../assets/images/img_main_card11.svg';
+import mainCardImage4 from '../../assets/images/img_main_card12.svg';
+import mainCardImage1 from '../../assets/images/img_main_card9.svg';
+import mainCardBigImage from '../../assets/images/img_main_cardProduct_big5.svg';
+
+const mainCardImages1: string[] = [mainCardBigImage];
+const mainCardImages2: { img: string; text: string }[] = [
+  { img: mainCardImage1, text: '욕실 용품' },
+  { img: mainCardImage2, text: '침구류' },
+  { img: mainCardImage3, text: '정리 선반' },
+  { img: mainCardImage4, text: '실내 인테리어' },
+];
 
 const Home = () => {
   return (
-    <div>
-      Home
-      <Outlet />
-    </div>
+    <>
+      <div css={homeStyle}>
+        <div css={headerStyle}>
+          <Header />
+          <MainCarousel />
+        </div>
+
+        <div css={sectionStyle}>
+          <div css={cardStyle}>
+            <MainCard mainCardImages={mainCardImages1} />
+            <MainCard mainCardImages={mainCardImages2} />
+          </div>
+          <div css={scrollStyle}>
+            <HorizontalScroll title={'가전 제품 및 주방의 베스트셀러'} images={images} />
+          </div>
+          <div css={loginRecommendStyle}>
+            <BottomLoginRecommend />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
