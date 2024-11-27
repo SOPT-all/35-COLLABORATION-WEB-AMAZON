6391d208dcfa3240903db5e65dcfa48b117a1d62
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-11-27 15:41:13 +0900

    refactor: 코드리뷰 반영

--- amazon/src/assets/svg/IcToastCheck.tsx
+++ amazon/src/assets/svg/IcToastCheck.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgIcToastCheck = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 46 46" {...props}>
--- amazon/src/vite-env.d.ts
+++ amazon/src/vite-env.d.ts
@@ -1,11 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_API_BASE_URL: string;
-  // 다른 환경 변수를 추가하려면 아래에 작성합니다.
-  // readonly VITE_SOME_KEY: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
