ca756817b186f73ca78fc26fd2d2e7bbd94a57c4
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-11-29 02:22:34 +0900

    [Design/#50] 메인 페이지 UI 구현 (#51)
    * design: title 수정
    
    * desing: MainCard 사진 및 텍스트
    
    * desing: scrollImage 변경
    
    * refactor: image file

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 
 import { MainCarousel, BottomLoginRecommend, MainCard, HorizontalScroll } from '@components';
-import { images } from '@constants';
+import { productCardImages, scrollImages1, scrollImages2, scrollImages3, scrollImages4 } from '@constants';
 import { IcGradationBanner } from '@svg';
 
 import {
@@ -13,18 +13,61 @@ import {
   headerStyle,
   iconStyle,
 } from './Home.style';
-import mainCardImage2 from '../../assets/images/img_main_card10.svg';
-import mainCardImage3 from '../../assets/images/img_main_card11.svg';
-import mainCardImage4 from '../../assets/images/img_main_card12.svg';
-import mainCardImage1 from '../../assets/images/img_main_card9.svg';
-import mainCardBigImage from '../../assets/images/img_main_cardProduct_big5.svg';
+import mainCardBigImg5 from '../../assets/images/img_main_cardProduct_big1.svg';
+import mainCardBigImg2 from '../../assets/images/img_main_cardProduct_big2.svg';
+import mainCardBigImg3 from '../../assets/images/img_main_cardProduct_big3.svg';
+import mainCardBigImg4 from '../../assets/images/img_main_cardProduct_big4.svg';
+import mainCardBigImg1 from '../../assets/images/img_main_cardProduct_big5.svg';
 
-const mainCardImages1: string[] = [mainCardBigImage];
-const mainCardImages2: { img: string; text: string }[] = [
-  { img: mainCardImage1, text: '욕실 용품' },
-  { img: mainCardImage2, text: '침구류' },
-  { img: mainCardImage3, text: '정리 선반' },
-  { img: mainCardImage4, text: '실내 인테리어' },
+const mainCardBigImage1: string[] = [mainCardBigImg1];
+const mainCardBigImage2: string[] = [mainCardBigImg2];
+const mainCardBigImage3: string[] = [mainCardBigImg3];
+const mainCardBigImage4: string[] = [mainCardBigImg4];
+const mainCardBigImage5: string[] = [mainCardBigImg5];
+
+const mainCardImagesList = [
+  [
+    { img: productCardImages[0], text: '남성' },
+    { img: productCardImages[1], text: '여성' },
+    { img: productCardImages[2], text: '남아' },
+    { img: productCardImages[3], text: '여아' },
+  ],
+  [
+    { img: productCardImages[4], text: '욕실 용품' },
+    { img: productCardImages[5], text: '침구류' },
+    { img: productCardImages[6], text: '정리 선반' },
+    { img: productCardImages[7], text: '실내 인테리어' },
+  ],
+  [
+    { img: productCardImages[8], text: '주방 및 다이닝' },
+    { img: productCardImages[9], text: '주택 개조 용품' },
+    { img: productCardImages[10], text: '데코' },
+    { img: productCardImages[11], text: '침구 및 욕실 용품' },
+  ],
+  [
+    { img: productCardImages[12], text: '게임기 본체' },
+    { img: productCardImages[13], text: '노트북' },
+    { img: productCardImages[14], text: 'PC 본체' },
+    { img: productCardImages[15], text: '게이밍 악세사리' },
+  ],
+  [
+    { img: productCardImages[16], text: '시계' },
+    { img: productCardImages[17], text: '테블릿' },
+    { img: productCardImages[18], text: '헤드셋' },
+    { img: productCardImages[19], text: '스마트폰' },
+  ],
+  [
+    { img: productCardImages[20], text: '향수' },
+    { img: productCardImages[21], text: '네일 용품' },
+    { img: productCardImages[22], text: '메이크업' },
+    { img: productCardImages[23], text: '스킨케어' },
+  ],
+  [
+    { img: productCardImages[24], text: '거울' },
+    { img: productCardImages[25], text: '메이크업' },
+    { img: productCardImages[26], text: '브러시' },
+    { img: productCardImages[27], text: '스폰지' },
+  ],
 ];
 
 const Home = () => {
@@ -48,37 +91,37 @@ const Home = () => {
         {/* 카드 섹션 */}
         <section css={sectionStyle}>
           <section css={cardStyle}>
-            <MainCard mainCardImages={mainCardImages1} />
-            <MainCard mainCardImages={mainCardImages2} />
-            <MainCard mainCardImages={mainCardImages1} />
-            <MainCard mainCardImages={mainCardImages2} />
+            <MainCard title="50$ 미만의 홈 데코" mainCardImages={mainCardBigImage1} />
+            <MainCard title="시계 베스트" mainCardImages={mainCardImagesList[0]} />
+            <MainCard title="당신의 집에 꼭 필요한 물건들" mainCardImages={mainCardImagesList[1]} />
+            <MainCard title="$50 미만의 홈 신제품" mainCardImages={mainCardImagesList[2]} />
           </section>
           <section css={cardStyle}>
-            <MainCard mainCardImages={mainCardImages1} />
-            <MainCard mainCardImages={mainCardImages2} />
-            <MainCard mainCardImages={mainCardImages1} />
-            <MainCard mainCardImages={mainCardImages2} />
+            <MainCard title="게이밍 악세사리" mainCardImages={mainCardImagesList[3]} />
+            <MainCard title="패션 홀리데이 특가" mainCardImages={mainCardBigImage2} />
+            <MainCard title="$20 미만의 소소한 선물" mainCardImages={mainCardBigImage3} />
+            <MainCard title="내 게임 시작하기" mainCardImages={mainCardBigImage4} />
           </section>
 
           <section css={scrollStyle}>
-            <HorizontalScroll title={'가전 제품 및 주방의 베스트셀러'} images={images} />
+            <HorizontalScroll title={'가전 제품 및 주방의 베스트셀러'} images={scrollImages1} />
           </section>
           <section css={scrollStyle}>
-            <HorizontalScroll title={'가전 제품 및 주방의 베스트셀러'} images={images} />
+            <HorizontalScroll title={'신규 고객 해외 구매'} images={scrollImages2} />
           </section>
 
           <section css={cardStyle}>
-            <MainCard mainCardImages={mainCardImages1} />
-            <MainCard mainCardImages={mainCardImages2} />
-            <MainCard mainCardImages={mainCardImages1} />
-            <MainCard mainCardImages={mainCardImages2} />
+            <MainCard title="대한민국으로의 무료 배송" mainCardImages={mainCardBigImage5} />
+            <MainCard title="귀하의 쇼핑 내역에서 영감을 얻은 기프트 아이디어" mainCardImages={mainCardImagesList[4]} />
+            <MainCard title="나를 위한 뷰티 제품 알아보기" mainCardImages={mainCardImagesList[5]} />
+            <MainCard title="뷰티 루틴 한 단계 업그레이드" mainCardImages={mainCardImagesList[6]} />
           </section>
 
           <section css={scrollStyle}>
-            <HorizontalScroll title={'가전 제품 및 주방의 베스트셀러'} images={images} />
+            <HorizontalScroll title={'$20 미만 가정 인테리어 용품'} images={scrollImages3} />
           </section>
           <section css={scrollStyle}>
-            <HorizontalScroll title={'가전 제품 및 주방의 베스트셀러'} images={images} />
+            <HorizontalScroll title={'장난감 및 게임의 베스트셀러'} images={scrollImages4} />
           </section>
 
           <section css={loginRecommendStyle}>
